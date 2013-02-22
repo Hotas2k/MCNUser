c4dbe3ebb140a6a6af9700744f2acde2f18eaa37
Jonas Eriksson <jonas@pmg.se>
2013-02-22 15:00:31 +0100

    Hotfixes

--- src/MCNUser/Listener/Authentication/RememberMeCookieCreator.php
+++ src/MCNUser/Listener/Authentication/RememberMeCookieCreator.php
@@ -98,7 +98,7 @@ class RememberMeCookieCreator implements ListenerAggregateInterface
         $expires = $token->getValidUntil() ? $token->getValidUntil()->getTimestamp() : null;
 
         $this->response->getHeaders()->addHeader(
-            new SetCookie('remember_me', $hash, $expires)
+            new SetCookie('remember_me', $hash, $expires, '/')
         );
     }
 }
